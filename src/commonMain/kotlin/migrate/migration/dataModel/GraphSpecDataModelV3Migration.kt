c1b2a6014e7573a29fc9f83213e5c4eddb98ebdd
Ollie Knowles <ollie.knowles@neo4j.com>
2026-07-09 16:01:43 +0100

    ensure all necessary data model values are present after migration

--- src/commonMain/kotlin/migrate/migration/dataModel/GraphSpecDataModelV3Migration.kt
+++ src/commonMain/kotlin/migrate/migration/dataModel/GraphSpecDataModelV3Migration.kt
@@ -48,10 +48,10 @@ class GraphSpecDataModelV3Migration :
                 "graphSchemaRepresentation" to schemaMapOf(
                     "version" to "1.0.0",
                     "graphSchema" to schemaMapOf(
-                        "nodeLabels" toNotEmpty nodeData?.labelsMap,
-                        "relationshipTypes" toNotEmpty relData?.typesMap,
-                        "nodeObjectTypes" toNotEmpty nodeData?.objectTypes,
-                        "relationshipObjectTypes" toNotEmpty relData?.objectTypes,
+                        "nodeLabels" to (nodeData?.labelsMap ?: emptyList()),
+                        "relationshipTypes" to (relData?.typesMap ?: emptyList()),
+                        "nodeObjectTypes" to (nodeData?.objectTypes ?: emptyList()),
+                        "relationshipObjectTypes" to (relData?.objectTypes ?: emptyList()),
                         "constraints" to constraints,
                         "indexes" to indexes
                     )
