5d90b7aadccbb805450a20b4f51e94d5fa586f66
GregHib <greghib@users.noreply.github.com>
2026-08-14 12:09:18 +0100

    Add internal round-trip integration tests

---
+++ src/jvmTest/kotlin/migrate/migration/dataModel/InternalDataModelIT.kt
@@ -0,0 +1,39 @@
+package migrate.migration.dataModel
+
+import GraphSpec
+import org.junit.jupiter.api.DynamicTest.dynamicTest
+import org.junit.jupiter.api.TestFactory
+import java.io.File
+import kotlin.test.Test
+import kotlin.test.assertEquals
+
+class InternalDataModelIT {
+
+    @TestFactory
+    fun `Internal model round-trip`() = listOf(
+        "adventureworks-sales.yaml",
+        "chinook.yaml",
+        "dvd-rental.yaml",
+        "flights.yaml",
+        "ldbc.yaml",
+        "northwind.yaml",
+        "pandc.yaml",
+        "vector-embeddings.yaml"
+    ).map { name ->
+        dynamicTest(name.removeSuffix(".yaml")) {
+            val initial = File(End2EndMigrationTest::class.java.getResource("prod-like/$name")!!.path).readText()
+            val expected = File(End2EndMigrationTest::class.java.getResource("internal/$name")!!.path).readText()
+            val model = GraphSpec.Yaml.decodeFromString(initial)
+
+            model.internalise()
+            val internal = GraphSpec.Yaml.encodeToString(model)
+
+            assertEquals(expected, internal)
+
+            model.prettify()
+            val pretty = GraphSpec.Yaml.encodeToString(model)
+            assertEquals(initial, pretty)
+        }
+    }
+
+}
