4564906f59d6decd7f35a6c494a3715d21a44bf0
Patrik Gustafsson <patrik@acyclic.eu>
2026-08-19 16:00:56 +0200

    Add NodeConstraintProperties validator - constraint with no properties
    Checks that a node constraint has at least one property.
    Produces 'missing_node_constraint_properties'.
    
    Refs: IMP-1210

---
+++ src/commonMain/kotlin/validate/node/constraint/NodeConstraintProperties.kt
@@ -0,0 +1,44 @@
+/*
+ * Copyright (c) "Neo4j"
+ * Neo4j Sweden AB [https://neo4j.com]
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package validate.node.constraint
+
+import model.GraphModel
+import model.node.Node
+import model.node.NodeConstraint
+import validate.Issue
+import validate.node.NodeValidation
+
+object NodeConstraintProperties : NodeValidation {
+    override fun validateConstraint(
+        model: GraphModel,
+        nodeId: String,
+        node: Node,
+        constraintId: String,
+        constraint: NodeConstraint,
+        issues: MutableList<Issue>
+    ) {
+        if (constraint.properties.isEmpty()) {
+            issues.add(
+                Issue(
+                    code = "missing_node_constraint_properties",
+                    message = "Constraint '$constraintId' on node '$nodeId' has no properties",
+                    path = "nodes.$nodeId.constraints.$constraintId.properties"
+                )
+            )
+        }
+    }
+}
