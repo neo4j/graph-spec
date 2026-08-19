caa2c52868398a96bfea2fc447c473ddb4582e20
Patrik Gustafsson <patrik@acyclic.eu>
2026-08-19 16:01:54 +0200

    Add NodeCompositeConstraint validator - composite with <2 properties
    Checks that a composite constraint (>=2 properties or 1 property
    with no name) has at least 2 properties.
    Produces 'invalid_node_composite_constraint'.
    
    Refs: IMP-1210

---
+++ src/commonMain/kotlin/validate/node/constraint/NodeCompositeConstraint.kt
@@ -0,0 +1,47 @@
+/*
+ * Copyright (c) "Neo4j"
+ * Neo4j Sweden AB [https://neo4j.com]
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package validate.node.constraint
+
+import model.GraphModel
+import model.node.Node
+import model.node.NodeConstraint
+import model.type.ConstraintType
+import validate.Issue
+import validate.node.NodeValidation
+
+object NodeCompositeConstraint : NodeValidation {
+    override fun validateConstraint(
+        model: GraphModel,
+        nodeId: String,
+        node: Node,
+        constraintId: String,
+        constraint: NodeConstraint,
+        issues: MutableList<Issue>
+    ) {
+        val isComposite = constraint.properties.size >= 2 ||
+            (constraint.properties.size == 1 && constraint.name.isNullOrBlank())
+        if (isComposite && constraint.properties.size < 2) {
+            issues.add(
+                Issue(
+                    code = "invalid_node_composite_constraint",
+                    message = "Composite constraint '$constraintId' on node '$nodeId' requires at least 2 properties",
+                    path = "nodes.$nodeId.constraints.$constraintId.properties"
+                )
+            )
+        }
+    }
+}
