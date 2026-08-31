bb962de280ffa1a5403b7a301cb2e85ffc7bbf4d
Patrik Gustafsson <patrik@acyclic.eu>
2026-08-31 13:42:39 +0200

    Add TableFieldEmptyName validator

---
+++ src/commonMain/kotlin/validate/table/TableFieldEmptyName.kt
@@ -0,0 +1,43 @@
+/*
+ * Copyright (c) "Neo4j"
+ * Neo4j Sweden AB [https://neo4j.com]
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package validate.table
+
+import model.GraphModel
+import model.source.Table
+import model.source.TableField
+import validate.Issue
+
+object TableFieldEmptyName : TableValidation {
+    override fun validateTableField(
+        model: GraphModel,
+        tableId: String,
+        table: Table,
+        fieldId: String,
+        field: TableField,
+        issues: MutableList<Issue>
+    ) {
+        if (field.name.isNullOrBlank()) {
+            issues.add(
+                Issue(
+                    code = "missing_table_field_name",
+                    message = "Missing name for table field '$fieldId'",
+                    path = "tables.$tableId.fields.$fieldId.name"
+                )
+            )
+        }
+    }
+}
