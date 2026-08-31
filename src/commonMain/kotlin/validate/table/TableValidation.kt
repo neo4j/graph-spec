821323f5c7c83e09d270f7b337477b1e1eff3bac
Patrik Gustafsson <patrik@acyclic.eu>
2026-08-31 10:23:31 +0200

    Add TableValidation interface

---
+++ src/commonMain/kotlin/validate/table/TableValidation.kt
@@ -0,0 +1,50 @@
+/*
+ * Copyright (c) "Neo4j"
+ * Neo4j Sweden AB [https://neo4j.com]
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package validate.table
+
+import model.GraphModel
+import model.source.Table
+import model.source.TableField
+import validate.Issue
+import validate.Validation
+import kotlin.collections.iterator
+import kotlin.js.JsExport
+
+@JsExport
+interface TableValidation : Validation {
+    override fun validate(model: GraphModel, issues: MutableList<Issue>) {
+        for ((tableId, table) in model.tables) {
+            validateTable(model, tableId, table, issues)
+            for ((fieldId, field) in table.fields) {
+                validateTableField(model, tableId, table, fieldId, field, issues)
+            }
+        }
+    }
+
+    fun validateTable(model: GraphModel, tableId: String, table: Table, issues: MutableList<Issue>) {
+    }
+
+    fun validateTableField(
+        model: GraphModel,
+        tableId: String,
+        table: Table,
+        fieldId: String,
+        field: TableField,
+        issues: MutableList<Issue>
+    ) {
+    }
+}
