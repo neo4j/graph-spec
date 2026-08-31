2765ccba9a931e3a2724a67a0d70224b242a7e9a
Patrik Gustafsson <patrik@acyclic.eu>
2026-08-31 16:04:58 +0200

    Add NodePropertyEmptyName validator

---
+++ src/commonMain/kotlin/validate/node/NodePropertyEmptyName.kt
@@ -0,0 +1,36 @@
+/*
+ * Copyright (c) "Neo4j"
+ * Neo4j Sweden AB [https://neo4j.com]
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package validate.node
+
+import model.GraphModel
+import model.node.Node
+import model.property.Property
+import validate.Issue
+import validate.property.emptyPropertyName
+
+object NodePropertyEmptyName : NodeValidation {
+    override fun validateProperty(
+        model: GraphModel,
+        nodeId: String,
+        node: Node,
+        propertyId: String,
+        property: Property,
+        issues: MutableList<Issue>
+    ) {
+        emptyPropertyName(propertyId, property, "nodes.$nodeId")?.let { issues.add(it) }
+    }
+}
