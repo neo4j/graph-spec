937e2e18e79adbe0615b544320938c8289643b17
Patrik Gustafsson <patrik@acyclic.eu>
2026-08-31 13:44:28 +0200

    Add TableFieldDuplicateName validator

---
+++ src/commonMain/kotlin/validate/table/TableFieldDuplicateName.kt
@@ -0,0 +1,49 @@
+/*
+ * Copyright (c) "Neo4j"
+ * Neo4j Sweden AB [https://neo4j.com]
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package validate.table
+
+import model.GraphModel
+import model.source.Table
+import model.source.TableField
+import validate.Issue
+
+object TableFieldDuplicateName : TableValidation {
+    override fun validateTableField(
+        model: GraphModel,
+        tableId: String,
+        table: Table,
+        fieldId: String,
+        field: TableField,
+        issues: MutableList<Issue>
+    ) {
+        val name = field.name ?: return
+        if (name.isBlank()) return
+        // UPX findArrayDuplicates + includes flags ALL fields with a duplicated name
+        val isDuplicate = table.fields.any { (otherId, other) ->
+            otherId != fieldId && other.name == name
+        }
+        if (isDuplicate) {
+            issues.add(
+                Issue(
+                    code = "duplicate_table_field_name",
+                    message = "Duplicate field name '$name' in table '$tableId'",
+                    path = "tables.$tableId.fields.$fieldId.name"
+                )
+            )
+        }
+    }
+}
