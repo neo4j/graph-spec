51c75f723e877e738ac5c4a9a4547d1de1d836d4
GregHib <greghib@users.noreply.github.com>
2026-09-01 12:15:45 +0100

    Fix relationship validation naming

--- src/commonMain/kotlin/validate/relationship/RelationshipIndexOptions.kt
+++ src/commonMain/kotlin/validate/relationship/RelationshipIndexOptions.kt
@@ -17,12 +17,9 @@
 package validate.relationship
 
 import model.GraphModel
-import model.node.Node
-import model.node.NodeIndex
 import model.relationship.Relationship
 import model.relationship.RelationshipIndex
 import validate.Issue
-import validate.node.NodeValidation
 
 object RelationshipIndexOptions : RelationshipValidation {
     override fun validateIndex(
@@ -36,8 +33,8 @@ object RelationshipIndexOptions : RelationshipValidation {
         if (index.options != null && index.options.type != index.type) {
             issues.add(
                 Issue(
-                    code = "node_index_type_options_mismatch",
-                    message = "Cannot use options type '${index.options.type}' with node index '$indexId' " +
+                    code = "relationship_index_type_options_mismatch",
+                    message = "Cannot use options type '${index.options.type}' with relationship index '$indexId' " +
                         "type '${index.type}'",
                     path = "relationships.$relationshipId.indexes.$indexId.options"
                 )
