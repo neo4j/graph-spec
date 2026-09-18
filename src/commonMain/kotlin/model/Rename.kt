6b185a7af5da888392862726d11a7381ae33e057
Patrik Gustafsson <patrik@acyclic.eu>
2026-09-18 16:37:12 +0200

    Add assignIds() alongside identify() in Rename.kt

--- src/commonMain/kotlin/model/Rename.kt
+++ src/commonMain/kotlin/model/Rename.kt
@@ -86,6 +86,27 @@ internal object Rename {
     }
 
     /**
+     * Replaces every key in the MutableMap with a predictable stable id.
+     * Pushing existing keys into [Named.name]
+     *
+     * @param type The type of field in use to prefix the stable id e.g: node0, node1, node2 etc...
+     * @param parent The parent field type to avoid stable id conflicts in a global map node0:property1, node0:property1
+     * @return Map of original keys to their replacements
+     */
+    internal fun <T : Named> MutableMap<String, T>.assignIds(
+        type: String,
+        parent: String? = null
+    ): Map<String, String> {
+        var i = 0
+        return transformKeys(
+            parent = parent,
+            skip = { it.name != null },
+            newKey = { _, _ -> "$type${i++}" },
+            updateName = { og, node -> node.name = og }
+        )
+    }
+
+    /**
      * Snapshots the map, clears it, then re-inserts each entry under a new key while
      * tracking old->new mappings.
      *
